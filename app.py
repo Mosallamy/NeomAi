170d8fa3769ced3a4191618d1afe2c5ddad9526d
Khalid Elmesallamy <khalidalmosallamy@gmail.com>
2020-09-07 14:22:06 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify,url_for
+from datetime import datetime
+import pandas as pd
+import os
+import csv
+
+app = Flask(__name__)
+
+@app.template_filter("date")
+def date(dateInSecodns):
+    dateInSecodns = int(dateInSecodns)
+    return datetime.utcfromtimestamp(dateInSecodns).strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route("/")
+def train():
+    predictions = list()
+    with open("/Users/Mosallamy/Desktop/Development/neom/data/Predections.csv", "r") as file:
+        reader = csv.DictReader(file)
+        predictions = list(reader)
+
+    return render_template("index.html", predictions = predictions[:10])
+
+if __name__ == "__main__":
+    app.run(debug=True)
